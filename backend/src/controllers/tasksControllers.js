8a7e4f21eb0f9d4ea827ff1be7ce21597ae4b776
Huỳnh Như Trí <thanhhuynh1202@gmail.com>
2025-09-21 03:48:15 +0700

    feat(backend): add filter functionality for tasks and update file server

--- backend/src/controllers/tasksControllers.js
+++ backend/src/controllers/tasksControllers.js
@@ -1,67 +1,105 @@
 import Task from "../models/Task.js";
 
 export const getAllTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find().sort({createdAt: -1});
-        res.status(200).json(tasks);
-    } catch (error) {
-        console.log("Lỗi khi gọi getAllTasks!", error);
-        res.status(500).json({message: "Lỗi hệ thống!"});
+  const { filter = "today" } = req.query;
+  const now = new Date();
+  let startDate;
+
+  switch (filter) {
+    case "today": {
+      startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate()); // yyyy-mm-dd 00:00     break;
+      break;
     }
+    case "week": {
+      const mondayDate =
+        now.getDate() - (now.getDay() - 1) - (now.getDay() == 0 ? 7 : 0);
+      startDate = new Date(now.getFullYear(), now.getMonth(), mondayDate);
+      break;
+    }
+    case "month": {
+      startDate = new Date(now.getFullYear(), now.getMonth(), 1);
+      break;
+    }
+    case "all":
+    default: {
+      startDate = null;
+    }
+  }
+
+  const query = startDate ? { createdAt: { $gte: startDate } } : {};
+  try {
+    const result = await Task.aggregate([
+      { $match: query },
+      {
+        $facet: {
+          tasks: [{ $sort: { createdAt: -1 } }],
+          activeCount: [{ $match: { status: "active" } }, { $count: "count" }],
+          completeCount: [
+            { $match: { status: "complete" } },
+            { $count: "count" },
+          ],
+        },
+      },
+    ]);
+    const tasks = result[0].tasks;
+    const activeCount = result[0].activeCount[0]?.count || 0;
+    const completeCount = result[0].completeCount[0]?.count || 0;
+
+    res.status(200).json({ tasks, activeCount, completeCount });
+  } catch (error) {
+    console.log("Lỗi khi gọi getAllTasks!", error);
+    res.status(500).json({ message: "Lỗi hệ thống!" });
+  }
 };
 
 export const createTask = async (req, res) => {
-    try {
-        const {title} = req.body;
-        const task = new Task({title});
+  try {
+    const { title } = req.body;
+    const task = new Task({ title });
 
-        const newTask = await task.save();
-        res.status(201).json(newTask);
-
-    } catch (error) {
-        console.log("Lỗi khi gọi createTask!", error);
-        res.status(500).json({message: "Lỗi hệ thống!"});
-    }
-    
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (error) {
+    console.log("Lỗi khi gọi createTask!", error);
+    res.status(500).json({ message: "Lỗi hệ thống!" });
+  }
 };
 
 export const updateTask = async (req, res) => {
-    try {
-        const {title, status, completedAt} = req.body;
-        const updateTask = await Task.findByIdAndUpdate(
-            req.params.id,
-            {
-                title,
-                status,
-                completedAt
-            },
-            {new : true}
-        );
-        
-        if(!updateTask){
-            return res.status(404).json({message: "Nhiệm vụ không tồn tại!"})
-        }
+  try {
+    const { title, status, completedAt } = req.body;
+    const updateTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        status,
+        completedAt,
+      },
+      { new: true }
+    );
 
-        res.status(201).json(updateTask);
-
-    } catch (error) {
-        console.log("Lỗi khi gọi updateTask!", error);
-        res.status(500).json({message: "Lỗi hệ thống!"});
+    if (!updateTask) {
+      return res.status(404).json({ message: "Nhiệm vụ không tồn tại!" });
     }
-    
+
+    res.status(201).json(updateTask);
+  } catch (error) {
+    console.log("Lỗi khi gọi updateTask!", error);
+    res.status(500).json({ message: "Lỗi hệ thống!" });
+  }
 };
 
 export const deleteTasks = async (req, res) => {
-    try {
-        const deleteTask = await Task.findByIdAndDelete(req.params.id);
+  try {
+    const deleteTask = await Task.findByIdAndDelete(req.params.id);
 
-        if(!deleteTask){
-            return res.status(404).json({message: "Nhiệm vụ không tồn tại!"});
-        }
-
-        res.status(201).json(deleteTask);
-    } catch (error) {
-        console.log("Lỗi khi gọi deleteTask!", error);
-        res.status(500).json({message: "Lỗi hệ thống!"});
+    if (!deleteTask) {
+      return res.status(404).json({ message: "Nhiệm vụ không tồn tại!" });
     }
+
+    res.status(201).json(deleteTask);
+  } catch (error) {
+    console.log("Lỗi khi gọi deleteTask!", error);
+    res.status(500).json({ message: "Lỗi hệ thống!" });
+  }
 };
